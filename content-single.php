92023b989ae3b763eee50658afb298fd82e47994
lacymorrow <me@lacymorrow.com>
2014-03-22 00:50:44 -0400

    newfile

--- content-single.php
+++ content-single.php
@@ -32,7 +32,7 @@
 		?>
     <header class="post-header">
         <?php if ( 'post' == get_post_type() ) : ?>
-			<span class="post-meta"><?php casper_posted_on(); ?> | <?php printf($category_list); echo " in "; printf($tag_list); ?></span>
+			<span class="post-meta"><?php casper_posted_on(); ?> | <?php printf($category_list); if ( '' != $tag_list ) { echo " in "; } printf($tag_list); ?></span>
 		<?php endif; ?>
         <h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
         <?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it. 
