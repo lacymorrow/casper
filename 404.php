748e4ef6974c93cef5d07965e996d9e7274b4118
ebinnion <ericbinnion@gmail.com>
2014-04-18 20:41:33 -0500

    correct 404 content width

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main hentry" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
