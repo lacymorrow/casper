ee167de30f4ae481cc64147375ca9a12f6743540
Moacir de Oliveira <moacirdeoliveira.eng@gmail.com>
2014-05-10 10:33:43 -0400

    Fix content position for content-none template
    Signed-off-by: Moacir de Oliveira <moacirdeoliveira.eng@gmail.com>

--- content-none.php
+++ content-none.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found hentry">
 	<header class="page-header">
 		<h1 class="page-title"><?php _e( 'Nothing Found', 'casper' ); ?></h1>
 	</header><!-- .page-header -->
