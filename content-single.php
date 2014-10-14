c595214ac0018769c1c2e7703c51d25e87f18111
Lacy Morrow <me@lacymorrow.com>
2014-10-14 14:28:05 -0700

    Fix #50; Add spacing in tags/categories

--- content-single.php
+++ content-single.php
@@ -31,11 +31,11 @@
 		?>
     <header class="post-header">
         <?php if ( 'post' == get_post_type() ) : ?>
-			<span class="post-meta"><?php casper_posted_on(); ?> | <?php printf($category_list); if ( '' != $tag_list ) { _e('in', 'casper'); } printf($tag_list); 
+			<span class="post-meta"><?php casper_posted_on(); ?> | <?php printf($category_list); if ( '' != $tag_list ) { _e(' in ', 'casper'); } printf($tag_list);
 			edit_post_link( __( 'Edit&rarr;', 'casper' ), '<span class="edit-link">&nbsp;&bull;&nbsp;', '</span>' );?></span>
 		<?php endif; ?>
         <h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-        <?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it. 
+        <?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
         	$image_id = get_post_thumbnail_id();
         	$thumb_url = wp_get_attachment_image_src($image_id,'thumbnail', true);
 			$medium_url = wp_get_attachment_image_src($image_id,'medium', true);
