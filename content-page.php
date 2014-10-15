07b5b53e22e0754146496a715cf5ae662bed3604
Lacy Morrow <me@lacymorrow.com>
2014-10-14 22:42:12 -0700

    Fix 31; Fix 54; All post now use <h1>

--- content-page.php
+++ content-page.php
@@ -11,7 +11,7 @@
         <?php if ( 'post' == get_post_type() ) : ?>
             <span class="post-meta"><?php casper_posted_on(); ?> | <?php printf($category_list); if ( '' != $tag_list ) { _e(' in ', 'casper'); } printf($tag_list); ?></span>
         <?php endif; ?>
-        <h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+        <h1 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
         <?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
             $image_id = get_post_thumbnail_id();
             $thumb_url = wp_get_attachment_image_src($image_id,'thumbnail', true);
