eb43466de047eca069500f4b3d75d5a89d8003d0
Lacy Morrow <me@lacymorrow.com>
2014-10-14 22:42:12 -0700

    Fix #31; Fix #54; All post now use <h1>

--- content-single.php
+++ content-single.php
@@ -34,7 +34,7 @@
 			<span class="post-meta"><?php casper_posted_on(); ?> | <?php printf($category_list); if ( '' != $tag_list ) { _e(' in ', 'casper'); } printf($tag_list);
 			edit_post_link( __( 'Edit&rarr;', 'casper' ), '<span class="edit-link">&nbsp;&bull;&nbsp;', '</span>' );?></span>
 		<?php endif; ?>
-        <h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+        <h1 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
         <?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
         	$image_id = get_post_thumbnail_id();
         	$thumb_url = wp_get_attachment_image_src($image_id,'thumbnail', true);
