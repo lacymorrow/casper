cb32614e911c76508efdd51ab10bbd4d5b136e2a
Maavuz Saif <maavuzsaif@gmail.com>
2016-02-16 23:05:14 +0500

    removed <title> tag
    removed <title> tag from header.php Since Version 4.1, themes should use add_theme_support() in the functions.php file in order to support title tag

--- header.php
+++ header.php
@@ -15,8 +15,6 @@
 <meta name="HandheldFriendly" content="True" />
 <meta name="MobileOptimized" content="320" />
 
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
