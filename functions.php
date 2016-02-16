71d9ef1169b96ca70345e2de112499e923e12eb6
Maavuz Saif <maavuzsaif@gmail.com>
2016-02-16 23:09:31 +0500

    added theme support for title tag.
    Let WordPress manage the document title. By adding theme support, we declare that this theme does not use a hard-coded <title> tag in the document head, and expect WordPress to provide it for us.

--- functions.php
+++ functions.php
@@ -38,6 +38,15 @@ function casper_setup() {
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
 	add_theme_support( 'post-thumbnails' );
+	
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+	
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
