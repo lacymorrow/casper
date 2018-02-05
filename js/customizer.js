1bd9b3eb8cb3e95c5cd52876c598ad14f78cd1db
Lacy Morrow <me@lacymorrow.com>
2018-02-04 22:11:59 -0800

    🚀jscs formatt

--- js/customizer.js
+++ js/customizer.js
@@ -6,120 +6,126 @@
 
 ( function( $ ) {
 
-	// Blog title and description.
-	wp.customize( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			$( '.blog-title a' ).text( to );
-		} );
-	} );
-	wp.customize( 'blogdescription', function( value ) {
-		value.bind( function( to ) {
-			$( '.blog-description' ).text( to );
-		} );
-	} );
-	/*
+    // Blog title and description.
+    wp.customize( 'blogname', function( value ) {
+        value.bind( function( to ) {
+            $( '.blog-title a' ).text( to );
+        } );
+    } );
+    wp.customize( 'blogdescription', function( value ) {
+        value.bind( function( to ) {
+            $( '.blog-description' ).text( to );
+        } );
+    } );
+    /*
 
-	// Header text color
-	wp.customize( 'header_textcolor', function( value ) {
-		value.bind( function( to ) {
-			if ( 'blank' === to || '' === to || false === to) {
-				$( 'header .blog-title a, header .blog-description, .social-icons a' ).css( {
-					'color' : 'inherit'
-				} );
-			} else {
-				$( 'header .blog-title a, header .blog-description, .social-icons a' ).css( {
-					'color': to
-				} );
-			}
-		} );
-	} );
-	// Hover color
-	wp.customize( 'casper_hover_color', function( value ) {
+    // Header text color
+    wp.customize( 'header_textcolor', function( value ) {
+        value.bind( function( to ) {
+            if ( 'blank' === to || '' === to || false === to) {
+                $( 'header .blog-title a, header .blog-description, .social-icons a' ).css( {
+                    'color' : 'inherit'
+                } );
+            } else {
+                $( 'header .blog-title a, header .blog-description, .social-icons a' ).css( {
+                    'color': to
+                } );
+            }
+        } );
+    } );
+    // Hover color
+    wp.customize( 'casper_hover_color', function( value ) {
         value.bind( function( to ) {
             $( 'a:hover, header .blog-title a:hover, header .social-icons a:hover' ).css( 'color', to );
         } );
     });
     // Home Menu color
-	wp.customize( 'casper_home_menu_color', function( value ) {
+    wp.customize( 'casper_home_menu_color', function( value ) {
         value.bind( function( to ) {
             $( '.main-navigation a' ).css( 'color', to );
         } );
     });
     // Menu color
-	wp.customize( 'casper_menu_color', function( value ) {
+    wp.customize( 'casper_menu_color', function( value ) {
         value.bind( function( to ) {
             $( '.main-navigation a' ).css( 'color', to );
         } );
     });
-	*/
-	// Other Page Header text color
-	wp.customize( 'casper_header_textcolor', function( value ) {
-		value.bind( function( to ) {
-			if ( 'blank' === to || '' === to || false === to ) {
-				$( ' header .blog-title a,  header .blog-description,  .social-icons a' ).css( {
-					'color': 'inherit'
-				} );
-			} else {
-				$( ' header .blog-title a,  header .blog-description,  .social-icons a' ).css( {
-					'color': to
-				} );
-			}
-		} );
-	} );
-	// Header Color
-	wp.customize( 'casper_header_color', function( value ) {
+    */
+
+    // Other Page Header text color
+    wp.customize( 'casper_header_textcolor', function( value ) {
+        value.bind( function( to ) {
+            if ( 'blank' === to || '' === to || false === to ) {
+                $( ' header .blog-title a,  header .blog-description,  .social-icons a' ).css( {
+                    'color': 'inherit'
+                } );
+            } else {
+                $( ' header .blog-title a,  header .blog-description,  .social-icons a' ).css( {
+                    'color': to
+                } );
+            }
+        } );
+    } );
+
+    // Header Color
+    wp.customize( 'casper_header_color', function( value ) {
         value.bind( function( to ) {
             $( '.site-head' ).css( 'background-color', to );
         } );
     });
-	// Link color
-	wp.customize( 'casper_link_color', function( value ) {
+
+    // Link color
+    wp.customize( 'casper_link_color', function( value ) {
         value.bind( function( to ) {
             $( 'section a' ).css( 'color', to );
         } );
     });
+    
     // Circle logo
-	wp.customize( 'casper_logo_circle', function( value ) {
+    wp.customize( 'casper_logo_circle', function( value ) {
         value.bind( function( to ) {
-        	if ( '' === to ) {
-	            $( '.blog-logo img' ).css( { '-webkit-border-radius': '0',
-	        								'-moz-border-radius': '0',
-	        								'border-radius': '0'
-	        	} );
-        	} else {
-	            $( '.blog-logo img' ).css( { '-webkit-border-radius': '50%',
-	        								'-moz-border-radius': '50%',
-	        								'border-radius': '50%'
-	        	} );
-	        }
+            if ( '' === to ) {
+                $( '.blog-logo img' ).css( { '-webkit-border-radius': '0',
+                                            '-moz-border-radius': '0',
+                                            'border-radius': '0'
+                } );
+            } else {
+                $( '.blog-logo img' ).css( { '-webkit-border-radius': '50%',
+                                            '-moz-border-radius': '50%',
+                                            'border-radius': '50%'
+                } );
+            }
         } );
     });
+
     // Logo frame
-	wp.customize( 'casper_logo_frame', function( value ) {
+    wp.customize( 'casper_logo_frame', function( value ) {
         value.bind( function( to ) {
-        	if ( '' === to ) {
-	            $( '.blog-logo img' ).css( { 'border': 'none',
-											'-webkit-box-shadow': 'none',
-											'-moz-box-shadow': 'none',
-											'box-shadow': 'none'
-	        	} );
-        	} else {
-	            $( '.blog-logo img' ).css( { 'border': '3px solid white',
-	    									'-webkit-box-shadow': '0 1px 1px rgba(0,0,0,0.3)',
-	   										'-moz-box-shadow': '0 1px 1px rgba(0,0,0,0.3)',
-	   										'box-shadow': '0 1px 1px rgba(0,0,0,0.3);} )'
-	        	} );
-	        }
-	    });
+            if ( '' === to ) {
+                $( '.blog-logo img' ).css( { 'border': 'none',
+                                            '-webkit-box-shadow': 'none',
+                                            '-moz-box-shadow': 'none',
+                                            'box-shadow': 'none'
+                } );
+            } else {
+                $( '.blog-logo img' ).css( { 'border': '3px solid white',
+                                            '-webkit-box-shadow': '0 1px 1px rgba(0,0,0,0.3)',
+                                            '-moz-box-shadow': '0 1px 1px rgba(0,0,0,0.3)',
+                                            'box-shadow': '0 1px 1px rgba(0,0,0,0.3);} )'
+                } );
+            }
+        });
     });
+    
     // Footer info
-	wp.customize( 'casper_custom_footer', function( value ) {
+    wp.customize( 'casper_custom_footer', function( value ) {
         value.bind( function( to ) {
-        	if ( '' === to ) {
-	            $( '.copyright' ).html( '<a href="https://github.com/lacymorrow/casper/" rel="home">Casper</a> by Lacy Morrow' );
-        	} else {
-	            $( '.copyright' ).html( to );
-	        }
+            if ( '' === to ) {
+                $( '.copyright' ).html( '<a href="https://github.com/lacymorrow/casper/" rel="home">Casper</a> by Lacy Morrow' );
+            } else {
+                $( '.copyright' ).html( to );
+            }
         } );
     });
 } )( jQuery );
--- src/js/casper.js
+++ src/js/casper.js
@@ -3,88 +3,90 @@
  */
 
 /*globals jQuery, document */
-(function (window, $) {
+(function( window, $ ) {
     'use strict';
 
     /*
     // @name: Responsive-img.js
     // @version: 1.1
-    // 
+    //
     // Copyright 2013-2014 Koen Vendrik, http://kvendrik.com
     // Licensed under the MIT license
     */
-    var makeImagesResponsive = function () {
+    var makeImagesResponsive = function() {
         var e = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
-            t = document.getElementsByTagName('body')[0].getElementsByTagName('img'),
+            t = document.getElementsByTagName( 'body' )[0].getElementsByTagName( 'img' ),
             r = window.devicePixelRatio ? window.devicePixelRatio >= 1.2 ? 1 : 0 : 0,
             i,
             n;
-        if (t.length === 0) {
+        if ( t.length === 0 ) {
             return;
         }
-        n = t[0].hasAttribute ? function (e, t) {
-            return e.hasAttribute(t);
-        } : function (e, t) {
-            return e.getAttribute(t) !== null;
+        n = t[0].hasAttribute ? function( e, t ) {
+            return e.hasAttribute( t );
+        } : function( e, t ) {
+            return e.getAttribute( t ) !== null;
         };
-        for (i = 0; i < t.length; i++) {
+        for ( i = 0; i < t.length; i++ ) {
             var s = t[i],
-                o = r && n(s, 'data-src2x') ? 'data-src2x' : 'data-src',
-                u = r && n(s, 'data-src-base2x') ? 'data-src-base2x' : 'data-src-base';
-            s.onload = function () {
+                o = r && n( s, 'data-src2x' ) ? 'data-src2x' : 'data-src',
+                u = r && n( s, 'data-src-base2x' ) ? 'data-src-base2x' : 'data-src-base';
+            s.onload = function() {
                 this.style.opacity = '1';
             };
-            if (!n(s, o)) { continue; }
-            var a = n(s, u) ? s.getAttribute(u) : '',
-                f = s.getAttribute(o),
-                l = f.split(',');
-            for (var c = 0; c < l.length; c++) {
-                var h = l[c].replace(':', '||').split('||'),
+            if ( ! n( s, o ) ) {
+ continue;
+ }
+            var a = n( s, u ) ? s.getAttribute( u ) : '',
+                f = s.getAttribute( o ),
+                l = f.split( ',' );
+            for ( var c = 0; c < l.length; c++ ) {
+                var h = l[c].replace( ':', '||' ).split( '||' ),
                     p = h[0],
                     d = h[1],
                     v, m;
-                if (p.indexOf('<') !== -1) {
-                    v = p.split('<');
-                    if (l[c - 1]) {
-                        var g = l[c - 1].split(/:(.+)/),
-                            y = g[0].split('<');
+                if ( p.indexOf( '<' ) !== -1 ) {
+                    v = p.split( '<' );
+                    if ( l[c - 1] ) {
+                        var g = l[c - 1].split( /:(.+)/ ),
+                            y = g[0].split( '<' );
                         m = e <= v[1] && e > y[1];
-                    } else { 
+                    } else {
                         m = e <= v[1];
                     }
                 } else {
-                    v = p.split('>');
-                    if (l[c + 1]) {
-                        var b = l[c + 1].split(/:(.+)/),
-                            w = b[0].split('>');
+                    v = p.split( '>' );
+                    if ( l[c + 1] ) {
+                        var b = l[c + 1].split( /:(.+)/ ),
+                            w = b[0].split( '>' );
                         m = e >= v[1] && e < w[1];
                     } else {
                         m = e >= v[1];
                     }
                 }
-                if (m) {
-                    var E = d.indexOf('//') !== -1 ? 1 : 0,
+                if ( m ) {
+                    var E = d.indexOf( '//' ) !== -1 ? 1 : 0,
                         S;
                     E === 1 ? S = d : S = a + d;
-                    s.src !== S && s.setAttribute('src', S);
+                    s.src !== S && s.setAttribute( 'src', S );
                     break;
                 }
             }
         }
     };
 
-    var init = function () {
-        $('.post-content').fitVids();
-        $(window).load(function () {
+    var init = function() {
+        $( '.post-content' ).fitVids();
+        $( window ).load(function() {
             makeImagesResponsive();
         });
     };
 
-    $(document).ready(function() {
+    $( document ).ready(function() {
         init();
     });
 
     return {
     };
 
-}(window, jQuery));
+}( window, jQuery ) );
